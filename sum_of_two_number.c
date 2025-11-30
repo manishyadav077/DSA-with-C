b279f32a9b9eabcb6624e4f614bf5c20c6f09e06
manishyadav077 <136573039+manishyadav077@users.noreply.github.com>
2025-11-30 21:31:23 +0545

    fixed the error

--- sum_of_two_number.c
+++ sum_of_two_number.c
@@ -2,10 +2,13 @@
 
 int main(){
 
-    
+    int a, b;
 
-    printf("enter a and b");
-    scanf("%d", &a, &b);
+    printf("enter a");
+    scanf("%d", &a);
+
+    printf("enter b");
+    scanf("%d", &b);
 
     
     printf("sum is: %d", a + b);
